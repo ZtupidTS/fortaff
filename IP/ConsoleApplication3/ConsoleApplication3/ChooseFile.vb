e17fbfbce9b3301fb764ffb0987f78cbbdadd4ff
ricain59 <ricain59@gmail.com>
2015-07-06 11:10:09 +0100

    - ip

---
+++ IP/ConsoleApplication3/ConsoleApplication3/ChooseFile.vb
@@ -0,0 +1,78 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text
+
+Public Class ChooseFile
+
+    Public ficheiro As String
+    Public qtdart As Integer
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Btnchoosefile.Click
+
+        OFDChooseFile.InitialDirectory = "C:\"
+        OFDChooseFile.Title = "Abrir um ficheiro  (txt)"
+        OFDChooseFile.Filter = "Text Files(*.txt)|*.txt"
+        OFDChooseFile.ShowDialog()
+        TBxUrlFile.Text = OFDChooseFile.FileName
+        ficheiro = OFDChooseFile.FileName
+    End Sub
+
+    Private Sub BtnImport_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnImport.Click
+        Dim textline, texterros As String
+        texterros = ""
+        'essa variavel vai me servir para quando eu fizer o split dos ;
+        Dim strArray() As String
+        Dim qtdart As Integer
+        qtdart = 0
+        
+        Dim objReader As New System.IO.StreamReader(ficheiro)
+
+        'VOU ABRIR O MOTOR PRIMAVERA
+        Try
+            Dim motor As ErpBS800.ErpBS
+            motor = New ErpBS800.ErpBS()
+            'abre a empresa
+            motor.AbreEmpresaTrabalho(StdBE800.EnumTipoPlataforma.tpProfissional, "FAFEDIS", "User", "1")
+
+            'aqui é meu ciclo para ler o meu ficheiro dos artigos
+            Do While objReader.Peek() <> -1
+
+                'li a linha do meu ficheiro
+                textline = objReader.ReadLine()
+                strArray = Split(textline, ";")
+                If Len(strArray(0)).Equals(13) Then
+                    'aqui é que vou criar no primavera
+                    If Not (motor.Comercial.Artigos.Existe(strArray(0))) Then
+                        ArticleToPrimavera.Main(strArray(0), strArray(1), motor)
+                        qtdart = qtdart + 1
+                    End If
+                Else
+                    'aqui devia guardar as linhas com erros
+                    texterros = texterros & textline & vbNewLine
+                End If
+            Loop
+
+            'aqui vou excrever num ficheiro as minhas linhas que não foram criados no primavera
+            If texterros <> "" Then
+                'aqui crio o ficheiro de erros
+                Dim path As String = "C:\erros create article.txt"
+                Dim fs As FileStream = File.Create(path)
+                fs.Close()
+                'aqui vou escrever nele
+                Dim objWriter As New System.IO.StreamWriter(path)
+                objWriter.Write(texterros)
+                objWriter.Close()
+            End If
+
+            'fecho a empresa
+            motor.FechaEmpresaTrabalho()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        MsgBox("Importação Concluído, foram criados " & qtdart & " artigos")
+
+    End Sub
+
+End Class
---
+++ IP/ConsoleApplication3/ConsoleApplication3/Inicial.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Inicial
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnImportArticle = New System.Windows.Forms.Button()
+        Me.BtnSupplier = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnImportArticle
+        '
+        Me.BtnImportArticle.Location = New System.Drawing.Point(51, 35)
+        Me.BtnImportArticle.Name = "BtnImportArticle"
+        Me.BtnImportArticle.Size = New System.Drawing.Size(196, 23)
+        Me.BtnImportArticle.TabIndex = 0
+        Me.BtnImportArticle.Text = "Criar Artigos"
+        Me.BtnImportArticle.UseVisualStyleBackColor = True
+        '
+        'BtnSupplier
+        '
+        Me.BtnSupplier.Location = New System.Drawing.Point(51, 64)
+        Me.BtnSupplier.Name = "BtnSupplier"
+        Me.BtnSupplier.Size = New System.Drawing.Size(196, 23)
+        Me.BtnSupplier.TabIndex = 1
+        Me.BtnSupplier.Text = "Alterações Fornecedor"
+        Me.BtnSupplier.UseVisualStyleBackColor = True
+        Me.BtnSupplier.UseWaitCursor = True
+        '
+        'Inicial
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 266)
+        Me.Controls.Add(Me.BtnSupplier)
+        Me.Controls.Add(Me.BtnImportArticle)
+        Me.Name = "Inicial"
+        Me.Text = "Inicial"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents BtnImportArticle As System.Windows.Forms.Button
+    Friend WithEvents BtnSupplier As System.Windows.Forms.Button
+End Class
